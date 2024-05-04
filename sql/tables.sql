e6e2e57e1ecf693c2f9694fb9f6fefbc82e77c39
Shaun Amott <shaun@inerd.com>
2024-05-04 18:02:31 +0200

    Table updates
    - Set default currency amount values (0).
    - Use UUIDs for bet IDs (+ fix key refs).
    - Add timestamp for bets.

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,14 +1,16 @@
+DROP TABLE IF EXISTS `ledger`;
 DROP TABLE IF EXISTS `users`;
-DROP TABLE IF EXISTS `games`;
 DROP TABLE IF EXISTS `bets`;
+DROP TABLE IF EXISTS `games`;
+DROP TABLE IF EXISTS `balances`;
 
 CREATE TABLE `games` (
 	`id` uuid PRIMARY KEY NOT NULL,
-	`created` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3), 
+	`created` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
 );
 
 CREATE TABLE `bets` (
-	`id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`id` uuid PRIMARY KEY NOT NULL,
 	`wallet` char(42) NOT NULL,
 	`gameId` uuid NOT NULL,
 	`autoCashOut` Decimal(6, 2) NOT NULL DEFAULT 0,
@@ -17,19 +19,18 @@ CREATE TABLE `bets` (
 	`amountUsd` Decimal(19, 2) unsigned NOT NULL,
 	`winnings` Decimal(32, 18) unsigned NOT NULL,
 	`winningsUsd` Decimal(19, 2) unsigned NOT NULL,
-	FOREIGN KEY(`wallet`) REFERENCES `users`(`wallet`),
+	`created` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 	FOREIGN KEY(`gameId`) REFERENCES `games`(`id`),
-	UNIQUE(`wallet`, `gameId`)
+	UNIQUE (`wallet`, `gameId`)
 );
 
 CREATE TABLE `balances` (
 	`wallet` char(42) NOT NULL,
 	`currency` varchar(32) NOT NULL,
-	`gained` Decimal(32, 18) unsigned NOT NULL,
-	`spent` Decimal(32, 18) unsigned NOT NULL,
-	`balance` Decimal(32, 18) unsigned NOT NULL,
-	FOREIGN KEY(`wallet`) REFERENCES `users`(`wallet`),
-	UNIQUE(`wallet`, `gameId`)
+	`gained` Decimal(32, 18) unsigned NOT NULL DEFAULT 0,
+	`spent` Decimal(32, 18) unsigned NOT NULL DEFAULT 0,
+	`balance` Decimal(32, 18) unsigned NOT NULL DEFAULT 0,
+	UNIQUE (`wallet`, `currency`)
 );
 
 CREATE TABLE `ledger` (
@@ -38,6 +39,5 @@ CREATE TABLE `ledger` (
 	`currency` varchar(32) NOT NULL,
 	`change` Decimal(32, 18) NOT NULL,
 	`betId` uuid NOT NULL,
-	FOREIGN KEY(`betId`) REFERENCES `bets`(`id`),
-	FOREIGN KEY(`wallet`) REFERENCES `users`(`wallet`)
+	FOREIGN KEY(`betId`) REFERENCES `bets`(`id`)
 );
