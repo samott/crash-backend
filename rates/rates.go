98ee5cf59ff570e3c6add7e741ef804b140965c8
Shaun Amott <shaun@inerd.com>
2024-05-12 18:11:58 +0200

    Add initial rates service + tests.

---
+++ rates/rates.go
@@ -0,0 +1,116 @@
+package rates
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+	"math"
+
+	"github.com/shopspring/decimal"
+);
+
+const FREE_API_URL = "https://api.coingecko.com/api";
+const PRO_API_URL = "https://pro-api.coingecko.com/api";
+
+/**
+ * ApiKey is the CoinGecko API key; use the empty string for basic API
+ * access (with lower access limits_
+ *
+ * Cryptos is a map of localId -> remoteId pairs.
+ *
+ * Fiats is an array of fiat currencies for which we want rates.
+ */
+type RatesConfig struct {
+	ApiKey string;
+	Cryptos map[string]string;
+	Fiats []string;
+}
+
+type Rates struct {
+	isPro bool;
+	config RatesConfig;
+}
+
+type FiatRates = map[string]decimal.Decimal;
+type RatesResult = map[string]FiatRates;
+
+func NewService(config *RatesConfig) (*Rates) {
+	return &Rates{
+		isPro: config.ApiKey != "",
+		config: *config,
+	};
+}
+
+func (rates *Rates) fetchRates() (RatesResult, error) {
+	localIds := make([]string, 0, len(rates.config.Cryptos))
+	remoteIds := make([]string, 0, len(rates.config.Cryptos))
+	remote2Local := make(map[string]string);
+
+	result := make(RatesResult);
+
+	req, _ := http.NewRequest("GET", rates.getUrl("/v3/simple/price"), nil);
+
+	for localId, remoteId := range rates.config.Cryptos {
+		localIds = append(localIds, localId);
+		remoteIds = append(remoteIds, remoteId);
+		remote2Local[remoteId] = localId;
+	}
+
+	query := req.URL.Query();
+	query.Add("ids", strings.Join(remoteIds, ","));
+	query.Add("vs_currencies", strings.Join(rates.config.Fiats, ","));
+	req.URL.RawQuery = query.Encode();
+
+	client := &http.Client{};
+
+	if rates.isPro {
+		req.Header.Set("X-CG-Pro-API-Key", rates.config.ApiKey);
+	}
+
+	res, err := client.Do(req);
+
+	if err != nil {
+		return result, err;
+	}
+
+	defer res.Body.Close();
+
+	if res.StatusCode > 299 || res.StatusCode < 200 {
+		return result, errors.New("API returned " + res.Status);
+	}
+
+	decoder := json.NewDecoder(res.Body);
+
+	var priceMap map[string]map[string]float64;
+
+	decoder.Decode(&priceMap);
+
+	if len(priceMap) != len(remoteIds) {
+		return result, errors.New("Failed to fetch all currencies");
+	}
+
+	for remoteId := range(priceMap) {
+		localId := remote2Local[remoteId];
+
+		result[localId] = make(FiatRates);
+
+		for fiatId, rate := range(priceMap[remoteId]) {
+			if (math.IsNaN(rate) || math.IsInf(rate, 0)) {
+				return result, nil;
+			}
+
+			result[localId][fiatId] = decimal.NewFromFloat(rate);
+		}
+	}
+
+	return result, nil;
+}
+
+func (rates *Rates) getUrl(path string) (string) {
+	if rates.isPro {
+		return PRO_API_URL + path;
+	} else {
+		return FREE_API_URL + path;
+	}
+}
---
+++ rates/rates_test.go
@@ -0,0 +1,134 @@
+package rates;
+
+import (
+	"testing"
+
+	"github.com/h2non/gock"
+	"github.com/shopspring/decimal"
+)
+
+func TestFetchRates(t *testing.T) {
+	defer gock.Off();
+
+	gock.New("https://api.coingecko.com").
+		Get("/api/v3/simple/price").
+		Reply(200).
+		BodyString(`{"bitcoin":{"usd":60000,"eur":50000},"ethereum":{"usd":3000,"eur":2000}}`);
+
+	config := RatesConfig{
+		ApiKey: "",
+		Cryptos: map[string]string{
+			"btc": "bitcoin",
+			"eth": "ethereum",
+		},
+		Fiats: []string{"usd", "eur"},
+	};
+
+	ratesSvc := NewService(&config);
+
+	result, err := ratesSvc.fetchRates()
+
+	if err != nil {
+		t.Fatal("Rates returned error", err);
+	}
+
+	if _, ok := result["btc"]; !ok { t.Fatal("Missing btc in result"); }
+	if _, ok := result["eth"]; !ok { t.Fatal("Missing eth in result"); }
+	if _, ok := result["btc"]["usd"]; !ok { t.Fatal("Missing btc:usd in result"); }
+	if _, ok := result["btc"]["eur"]; !ok { t.Fatal("Missing btc:eur in result"); }
+	if _, ok := result["eth"]["usd"]; !ok { t.Fatal("Missing eth:usd in result"); }
+	if _, ok := result["eth"]["eur"]; !ok { t.Fatal("Missing eth:eur in result"); }
+
+	if v, _ := decimal.NewFromString("60000"); !v.Equal(result["btc"]["usd"]) {
+		t.Fatal("btc:usd value not correct:", v, "vs.", result["btc"]["usd"]);
+	}
+
+	if v, _ := decimal.NewFromString("50000"); !v.Equal(result["btc"]["eur"]) {
+		t.Fatal("btc:eur value not correct:", v, "vs.", result["btc"]["eur"]);
+	}
+
+	if v, _ := decimal.NewFromString("3000"); !v.Equal(result["eth"]["usd"]) {
+		t.Fatal("eth:usd value not correct:", v, "vs.", result["eth"]["usd"]);
+	}
+
+	if v, _ := decimal.NewFromString("2000"); !v.Equal(result["eth"]["eur"]) {
+		t.Fatal("eth:eur value not correct:", v, "vs.", result["eth"]["eur"]);
+	}
+}
+
+func TestFetchRatesWithMissingCurrency(t *testing.T) {
+	defer gock.Off();
+
+	gock.New("https://api.coingecko.com").
+		Get("/api/v3/simple/price").
+		Reply(200).
+		BodyString(`{"bitcoin":{"usd":60000,"eur":50000}`);
+
+	config := RatesConfig{
+		ApiKey: "",
+		Cryptos: map[string]string{
+			"btc": "bitcoin",
+			"eth": "ethereum",
+		},
+		Fiats: []string{"usd", "eur"},
+	};
+
+	ratesSvc := NewService(&config);
+
+	_, err := ratesSvc.fetchRates()
+
+	if err == nil {
+		t.Fatal("Rates improperly accepted result with missing currency", err);
+	}
+}
+
+func TestFetchRatesWithMissingFiat(t *testing.T) {
+	defer gock.Off();
+
+	gock.New("https://api.coingecko.com").
+		Get("/api/v3/simple/price").
+		Reply(200).
+		BodyString(`{"bitcoin":{"usd":60000}`);
+
+	config := RatesConfig{
+		ApiKey: "",
+		Cryptos: map[string]string{
+			"btc": "bitcoin",
+		},
+		Fiats: []string{"usd", "eur"},
+	};
+
+	ratesSvc := NewService(&config);
+
+	_, err := ratesSvc.fetchRates()
+
+	if err == nil {
+		t.Fatal("Rates improperly accepted result with missing fiat currency", err);
+	}
+}
+
+func TestFetchRatesWithInvalid(t *testing.T) {
+	defer gock.Off();
+
+	gock.New("https://api.coingecko.com").
+		Get("/api/v3/simple/price").
+		Reply(200).
+		BodyString(`{"bitcoin":{"usd":"wrong","eur":20000}`);
+
+	config := RatesConfig{
+		ApiKey: "",
+		Cryptos: map[string]string{
+			"btc": "bitcoin",
+			"eth": "ethereum",
+		},
+		Fiats: []string{"usd", "eur"},
+	};
+
+	ratesSvc := NewService(&config);
+
+	_, err := ratesSvc.fetchRates()
+
+	if err == nil {
+		t.Fatal("Rates improperly parsed invalid response", err);
+	}
+}
