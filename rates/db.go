86933b41e08871a991680011e736148dc65ad6ca
Shaun Amott <shaun@inerd.com>
2024-06-11 22:18:38 +0200

    Add missing query argument.

--- rates/db.go
+++ rates/db.go
@@ -7,10 +7,11 @@ import (
 func (rates *Rates) SaveRates(prices RatesResult, db *sql.DB) (error) {
 	for cryptoId := range prices {
 		for fiatId, rate := range prices[cryptoId] {
+			rateStr := rate.String();
 			_, err := db.Exec(`
 				INSERT INTO rates (base, target, ratio) VALUES (?, ?, ?)
 				ON DUPLICATE KEY UPDATE ratio = ?
-			`, cryptoId, fiatId, rate); 
+			`, cryptoId, fiatId, rateStr, rateStr);
 
 			if err != nil {
 				return err;
